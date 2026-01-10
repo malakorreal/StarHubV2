893f1efbf7534ae8498296d7ca5cc7db5020702f
StarHub Developer <dev@starhub.com>
2026-01-10 15:58:38 +0700

    Bump version to 1.0.20 and fix update button logic

--- src/renderer/src/locales/th.js
+++ src/renderer/src/locales/th.js
@@ -14,7 +14,7 @@ export default {
   main: {
     selectInstance: "เลือก Instance ที่ต้องการเล่น",
     serverInfo: "ข้อมูลเซิร์ฟเวอร์",
-    readyToPlay: "พร้อมลุย! (v1.0.19)",
+    readyToPlay: "พร้อมลุย! (v1.0.20)",
     announcement: "ประกาศ",
     version: "เวอร์ชัน",
     play: "เล่นเกม",
