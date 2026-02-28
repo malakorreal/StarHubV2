443c29d0ebafa1dc345e23893c150d3eca544dba
StarHub Developer <dev@starhub.com>
2026-02-28 11:17:38 +0700

    feat: switch to Kanit font and fix dark mode

--- web-admin/src/app/[locale]/layout.js
+++ web-admin/src/app/[locale]/layout.js
@@ -1,17 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kanit } from "next/font/google";
 import "../globals.css";
 import { Providers } from "../providers";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const kanit = Kanit({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ["latin", "thai"],
+  variable: "--font-kanit",
 });
 
 export const metadata = {
@@ -25,7 +21,7 @@ export default async function LocaleLayout({ children, params: { locale } }) {
   return (
     <html lang={locale} suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kanit.variable} antialiased font-sans`}
       >
         <NextIntlClientProvider messages={messages}>
           <Providers>
